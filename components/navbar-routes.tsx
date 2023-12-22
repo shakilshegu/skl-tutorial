fe26e4f22c9ab4a7f809e43be6b1501de488751f
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-22 23:03:23 +0530

    signout and teacher button

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,36 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
+import { usePathname, useRouter } from "next/navigation"
+import { LogOut } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 const NavbarRoutes = () => {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter")
+
   return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton/>
+      {isTeacherPage || isPlayerPage ? (
+        <Button>
+          <LogOut className="h-4 w-4 mr-2 ">
+            Exit
+          </LogOut>
+        </Button>
+      ):(
+        <Link href="/teacher/courses">
+        <Button size="sm" variant="ghost">
+          Teacher Mode
+        </Button>
+        </Link>
+      )}
+      <UserButton
+       afterSignOutUrl="/"
+      />
     </div>
   )
 }
