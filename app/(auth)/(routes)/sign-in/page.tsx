2a2eb93de640ff7f807278f32081111c87394808
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-22 15:19:51 +0530

    .env and page and clerk setup

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>THis May New Pages </div>
+  )
+}
+
+export default page
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,12 +1,8 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+
 
 export default function Home() {
   return (<div>
     <p className='text-3xl font-medium text-sky-500'>Hellow world</p>
-    <Button >
-      Click me
-    </Button>
 
   </div>
 
