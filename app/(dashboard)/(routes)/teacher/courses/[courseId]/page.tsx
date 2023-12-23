cb588c6ec1a0535d93a87978032086dd3b8884a2
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-23 15:37:05 +0530

    icone and cours page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,4 +1,6 @@
 import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 
 
 const CourseIdPage = async ({
@@ -6,15 +8,49 @@ const CourseIdPage = async ({
 }: {
     params: { courseId: string }
 }) => {
+    const { userId } = auth()
+    if(!userId){
+        return redirect("/");
+    }
     const course = await db.course.findUnique({
-        where:{
-            id:params.courseId
+        where: {
+            id: params.courseId
         }
     })
+    if(!course){
+        return redirect("/");
+    }
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.price,
+        course.categoryId,
+    ];
+    const totalFields = requiredFields.length;
+    const compeltedFields = requiredFields.filter(Boolean).length
+
+    const completionText = `(${compeltedFields}/${totalFields})`
 
     return (
-        <div>
-           Course Id :{params.courseId}
+        <div className="p-6">
+           <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">
+              Course setup
+                </h1>
+                <span className="text-sm text-slate-700">
+                    Complete all fields {completionText}
+                </span>
+            </div>
+           </div>
+           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+             <div className="flex  items-center gap-x-2">
+              <h2 className="text-xl ">
+                Customize your course
+              </h2>
+             </div>
+           </div>
         </div>
     )
 }
