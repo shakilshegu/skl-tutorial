c014623b4401cf33f6027eda7a00b1753fbe224c
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-23 13:16:46 +0530

    Prisma setup

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -4,6 +4,7 @@ import * as z from "zod"
 import axios from "axios"
 import {zodResolver} from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 import Link from "next/link";
 import {
@@ -41,7 +42,7 @@ const CreatePage = () => {
             const response = await axios.post("/api/course",values)
             router.push(`/teacher/courses/${response.data.id}`);
         } catch (error) {
-            console.log(error);
+            toast.error("Somthing went wrong")
             
         }
     }
