8484120c269272862e25d1e8ba24556a26bf2e9f
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-23 16:28:28 +0530

    form setup

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,21 +1,55 @@
 "use client";
+import * as z from "zod"
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-interface TitleFormProps{
-    initialData:{
-        title:string
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage
+} from "@/components/ui/form"
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+
+
+interface TitleFormProps {
+    initialData: {
+        title: string
     };
-    courseId : string
+    courseId: string
 
 };
+
+const formSchema = z.object({
+    title: z.string().min(1, {
+        message: "Title is required"
+    })
+})
 
 const TitleForm = ({
     initialData,
     courseId
 
-}:TitleFormProps) => {
-  return (
-    <div>TitleForm</div>
-  )
+}: TitleFormProps) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData
+    });
+
+    const { isSubmitting, isValid } = form.formState;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+
+    }
+    return (
+        <div>TitleForm</div>
+    )
 }
 
 export default TitleForm
