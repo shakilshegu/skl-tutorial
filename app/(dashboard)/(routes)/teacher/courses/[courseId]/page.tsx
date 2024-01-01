e45217b0addc81a7847a86fdf30d995f5802ad27
shakilshagu <muhammadshakil1968@gmail.com>
2024-01-01 18:05:39 +0530

    chapters works

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import ImageForm from "./_components/image-form"
 import CategoryForm from "./_components/category-form"
 import { PriceForm } from "./_components/price-form"
 import  AttachmentForm  from "./_components/attachment-form"
+import ChaptersForm from "./_components/chapters-form"
 
 
 
@@ -27,6 +28,11 @@ const CourseIdPage = async ({
       userId
     },
     include:{
+      chapters:{
+         orderBy:{
+          position:"asc"
+         }
+      },
       attachments:{
         orderBy:{
           createdAt:"desc",
@@ -49,6 +55,7 @@ const CourseIdPage = async ({
     course.imageUrl,
     course.price,
     course.categoryId,
+    course.chapters.some(chapter => chapter.isPublished),
   ];
   const totalFields = requiredFields.length;
   const compeltedFields = requiredFields.filter(Boolean).length
@@ -106,9 +113,10 @@ const CourseIdPage = async ({
                   Course chapters
                 </h2>
               </div>
-
-              <div> TODO: Chapters
-              </div>
+              <ChaptersForm
+              initialData={course}
+              courseId={course.id}
+            />
             </div>
             <div>
               <div className="flex items-center gap-x-2">
