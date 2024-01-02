dcb7cea64f1f5958f17cfc7bcb36dc992619a582
shakilshagu <muhammadshakil1968@gmail.com>
2024-01-02 14:42:00 +0530

    page chapters set up

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { IconBadge } from "@/components/icon-badge";
 
 
 
@@ -48,12 +49,36 @@ const ChapterIdpage = async ({
                     <Link
                         href={`/teacher/courses/${params.courseId}`}
                         className="flex items-center text-sm hover:opacity-75 transition mb-6"
-                    />
-                    <ArrowLeft className="h-4 w-4 mr-2"
-                    />
-                    Back to course setup
+                    >
+                        <ArrowLeft className="h-4 w-4 mr-2"
+                        />
+                        Back to course setup
+                    </Link>
+                    <div className="flex items-center justify-between w-full">
+                        <div className="flex flex-col gap-y-2">
+                            <h1 className="text-2xl font-medium">
+                                Chapter Creation
+                            </h1>
+                            <span className="text-sm text-slate-700">
+                                Complete all fields {completionText}
+                            </span>
+                        </div>
+                    </div>
                 </div>
 
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div className="space-y-4">
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={LayoutDashboard} />
+                            <h2 className="text-xl">
+                                Customize your chapter
+                            </h2>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     )
